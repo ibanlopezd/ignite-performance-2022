483aa826ba16ad0939f05209b14def91ef5c9a21
Iban Lopez Delgado <iban@brasilfone.com.br>
2022-08-12 09:50:18 -0300

    [DVB-5431] - Componente Tabela transaçoes

--- dt-money/src/pages/Transactions/index.tsx
+++ dt-money/src/pages/Transactions/index.tsx
@@ -1,11 +1,38 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
     return (
         <div>
             <Header />
             <Summary />
+            <TransactionsContainer>
+                <TransactionsTable>
+                    <tbody>
+                        <tr>
+                            <td width='40%'>placeholder</td>
+                            <td>
+                                <PriceHighlight variant='income'>
+                                    $ 5000
+                                </PriceHighlight>
+                            </td>
+                            <td>placeholder</td>
+                            <td>placeholder</td>
+                        </tr>
+                        <tr>
+                            <td width='40%'>placeholder</td>
+                            <td>
+                                <PriceHighlight variant="outcome">
+                                    $ 500
+                                </PriceHighlight>
+                            </td>
+                            <td>placeholder</td>
+                            <td>placeholder</td>
+                        </tr>
+                    </tbody>
+                </TransactionsTable>
+            </TransactionsContainer>
         </div>
     )
 }
