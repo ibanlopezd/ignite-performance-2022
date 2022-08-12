dbea749af0a4ab1f4478fd8fc2bdbc23fe5a4c09
Iban Lopez Delgado <iban@brasilfone.com.br>
2022-08-12 15:10:55 -0300

    [DVB-5431] - Formatando valores

--- dt-money/src/components/Summary/index.tsx
+++ dt-money/src/components/Summary/index.tsx
@@ -2,6 +2,7 @@ import { SummaryContainer, SummaryCard} from "./styles";
 import { ArrowCircleUp, ArrowCircleDown, CurrencyDollar } from 'phosphor-react';
 import { useContext } from "react";
 import { TransactionsContext } from "../../contexts/TransactionContext";
+import { priceFormatter } from "../../utils/formatter";
 
 export function Summary() {
 
@@ -35,7 +36,7 @@ export function Summary() {
                     <ArrowCircleUp size={32} color='#00b37e' />
                 </header>
 
-                <strong>{summary.income}</strong>
+                <strong>{priceFormatter.format(summary.income)}</strong>
             </SummaryCard>
 
             <SummaryCard>
@@ -46,7 +47,7 @@ export function Summary() {
                     <ArrowCircleDown size={32} color='#f75a68' />
                 </header>
 
-                <strong>{summary.outcome}</strong>
+                <strong>{'- ' + priceFormatter.format(summary.outcome)}</strong>
             </SummaryCard>
 
             <SummaryCard variant="green">
@@ -57,7 +58,7 @@ export function Summary() {
                     <CurrencyDollar size={32} color='#ffffff' />
                 </header>
 
-                <strong>{summary.total}</strong>
+                <strong>{priceFormatter.format(summary.total)}</strong>
             </SummaryCard>
         </SummaryContainer>
     )
