08764eb4a84ef20bddaa305dfaae497ba9b21ba2
Iban Lopez Delgado <iban@brasilfone.com.br>
2022-08-12 17:14:44 -0300

    [DVB-5431] - Aplicando useMemo

--- dt-money/src/hooks/useSummary.ts
+++ dt-money/src/hooks/useSummary.ts
@@ -1,5 +1,6 @@
 import { useContextSelector } from 'use-context-selector'
 import { TransactionsContext } from '../contexts/TransactionContext'
+import { useMemo } from 'react'
 
 export function useSummary() {
   const transactions = useContextSelector(
@@ -7,24 +8,25 @@ export function useSummary() {
     (context) => context.transactions,
   )
 
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === 'income') {
-        acc.income += transaction.price
-        acc.total += transaction.price
-      } else {
-        acc.outcome += transaction.price
-        acc.total -= transaction.price
-      }
-
-      return acc
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    },
-  )
+  const summary = useMemo(() => {
+    return transactions.reduce(
+      (acc, transaction) => {
+        if (transaction.type === 'income') {
+          acc.income += transaction.price
+          acc.total += transaction.price
+        } else {
+          acc.outcome += transaction.price
+          acc.total -= transaction.price
+        }
+        return acc
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    )
+  }, [transactions])
 
   return summary
 }
